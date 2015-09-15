31690ce0470933c21c246ddadc473280e89a1b7b
gabemontero <gmontero@redhat.com>
2015-09-14 23:27:00 -0400

    add build verification step

---
+++ src/main/java/com/openshift/openshiftjenkinsbuildutils/OpenShiftBuildVerifier.java
@@ -0,0 +1,260 @@
+package com.openshift.openshiftjenkinsbuildutils;
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.util.FormValidation;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.AbstractProject;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
+
+import com.openshift.restclient.ClientFactory;
+import com.openshift.restclient.IClient;
+import com.openshift.restclient.ISSLCertificateCallback;
+import com.openshift.restclient.ResourceKind;
+import com.openshift.restclient.authorization.TokenAuthorizationStrategy;
+import com.openshift.restclient.capability.ICapability;
+import com.openshift.restclient.model.IBuild;
+
+import javax.net.ssl.SSLSession;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * OpenShift {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link OpenShiftBuildVerifier} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
+ * method will be invoked. 
+ *
+ * @author Gabe Montero
+ */
+public class OpenShiftBuildVerifier extends Builder implements ISSLCertificateCallback {
+	
+    private String apiURL = "https://openshift.default.svc.cluster.local";
+    private String bldCfg = "frontend";
+    private String nameSpace = "test";
+    private String authToken = "";
+    
+    
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public OpenShiftBuildVerifier(String apiURL, String bldCfg, String nameSpace, String authToken) {
+        this.apiURL = apiURL;
+        this.bldCfg = bldCfg;
+        this.nameSpace = nameSpace;
+        this.authToken = authToken;
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public String getApiURL() {
+		return apiURL;
+	}
+
+	public String getBldCfg() {
+		return bldCfg;
+	}
+
+	public String getNameSpace() {
+		return nameSpace;
+	}
+	
+	public String getAuthToken() {
+		return authToken;
+	}
+	
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+    	System.setProperty(ICapability.OPENSHIFT_BINARY_LOCATION, Constants.OC_LOCATION);
+    	listener.getLogger().println("OpenShiftBuildVerifier in perform for " + bldCfg);
+    	
+    	// obtain auth token from defined spot in OpenShift Jenkins image
+    	authToken = Auth.deriveAuth(authToken, listener);
+    	
+    	
+    	// get oc client (sometime REST, sometimes Exec of oc command
+    	IClient client = new ClientFactory().create(apiURL, this);
+    	
+    	if (client != null) {
+    		// seed the auth
+        	client.setAuthorizationStrategy(new TokenAuthorizationStrategy(this.authToken));
+        	
+			String bldState = null;
+			long currTime = System.currentTimeMillis();
+			while (System.currentTimeMillis() < (currTime + 60000)) {
+				List<IBuild> blds = client.list(ResourceKind.BUILD, nameSpace);
+				Map<String,IBuild> ourBlds = new HashMap<String,IBuild>();
+				List<String> ourKeys = new ArrayList<String>();
+				for (IBuild bld : blds) {
+					if (bld.getName().startsWith(bldCfg)) {
+						ourKeys.add(bld.getName());
+						ourBlds.put(bld.getName(), bld);
+					}
+				}
+				
+				if (ourKeys.size() > 0) {
+					Collections.sort(ourKeys);
+					IBuild bld = ourBlds.get(ourKeys.get(ourKeys.size() - 1));
+					listener.getLogger().println("OpenShiftBuildVerifier latest bld id " + ourKeys.get(ourKeys.size() - 1));
+					bldState = bld.getStatus();
+				}
+				
+				listener.getLogger().println("OpenShiftBuildVerifier post bld launch bld state:  " + bldState);
+				if (bldState == null || !bldState.equals("Complete")) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
+				} else {
+					break;
+				}
+			}
+			
+			if (bldState == null || !bldState.equals("Complete")) {
+				listener.getLogger().println("OpenShiftBuildVerifier build state is " + bldState + ".  If possible interrogate the OpenShift server with the oc command and inspect the server logs");
+				return false;
+			} else 
+				return true;
+    				        		
+    	} else {
+    		listener.getLogger().println("OpenShiftBuildVerifier could not get oc client");
+    		return false;
+    	}
+
+    }
+
+    public void setApiURL(String apiURL) {
+		this.apiURL = apiURL;
+	}
+
+	public void setBldCfg(String bldCfg) {
+		this.bldCfg = bldCfg;
+	}
+
+	public void setNameSpace(String nameSpace) {
+		this.nameSpace = nameSpace;
+	}
+
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+
+	// Overridden for better type safety.
+    // If your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link OpenShiftBuildVerifier}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     */
+    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        /**
+         * To persist global configuration information,
+         * simply store it in a field and call save().
+         *
+         * <p>
+         * If you don't want fields to be persisted, use <tt>transient</tt>.
+         */
+
+        /**
+         * In order to load the persisted global configuration, you have to 
+         * call load() in the constructor.
+         */
+        public DescriptorImpl() {
+            load();
+        }
+
+        /**
+         * Performs on-the-fly validation of the various fields.
+         *
+         * @param value
+         *      This parameter receives the value that the user has typed.
+         * @return
+         *      Indicates the outcome of the validation. This is sent to the browser.
+         *      <p>
+         *      Note that returning {@link FormValidation#error(String)} does not
+         *      prevent the form from being saved. It just means that a message
+         *      will be displayed to the user. 
+         */
+        public FormValidation doCheckApiURL(@QueryParameter String value)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set apiURL");
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckBldCfg(@QueryParameter String value)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set bldCfg");
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckNameSpace(@QueryParameter String value)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set nameSpace");
+            return FormValidation.ok();
+        }
+        
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // Indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Get lastest OpenShift build status";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            // To persist global configuration information,
+            // pull info from formData, set appropriate instance field (which should have a getter), and call save().
+            save();
+            return super.configure(req,formData);
+        }
+
+    }
+
+	@Override
+	public boolean allowCertificate(X509Certificate[] chain) {
+		return true;
+	}
+
+	@Override
+	public boolean allowHostname(String hostname, SSLSession session) {
+		return true;
+	}
+}
+
